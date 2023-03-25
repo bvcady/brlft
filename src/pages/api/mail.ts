28a817652a67ad5ea961ad8728dea55fa72ee7c1
Bob Verheij <bobverheij@Bobs-MBP-2.home>
2023-03-25 16:51:41 +0100

    Email fix

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,18 +1,23 @@
 import { NextApiRequest, NextApiHandler, NextApiResponse } from "next";
 
 import sgMail from "@sendgrid/mail";
+import { render } from "@react-email/components";
+import NotionMagicLinkEmail from "../../../emails";
 
 export const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
 
   if (method === "POST") {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const API_KEY = process.env.SENDGRID_API_KEY;
+    const emailHtml = render(NotionMagicLinkEmail({ token: "whatever this is" }));
+
+    sgMail.setApiKey(API_KEY);
     const msg = {
       to: "bobb.verheij@gmail.com", // Change to your recipient
       from: "brlft@brlft.nl", // Change to your verified sender
       subject: "Sending with SendGrid is Fun",
       text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+      html: emailHtml,
     };
     sgMail
       .send(msg)
